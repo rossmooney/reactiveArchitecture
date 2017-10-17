8f551d651dc2c18bef2a35cdafa03ee39fae0983
Ross Mooney <rossmooney@Rosss-MacBook-Pro.local>
2017-10-16 21:07:23 -0400

    Initial Commit

---
+++ ArchitectureTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArchitectureTest
+//
+//  Created by Ross Mooney on 10/16/17.
+//  Copyright © 2017 Comprendo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
