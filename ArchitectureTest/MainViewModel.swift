ef54c896a0a0de02db2c9495c470c8ffe77fbadc
Ross Mooney <rossmooney@Rosss-MacBook-Pro.local>
2017-10-16 22:13:29 -0400

    Project is now testable

--- ArchitectureTest/MainViewModel.swift
+++ ArchitectureTest/MainViewModel.swift
@@ -20,8 +20,20 @@ enum MainState {
     case error(MainScreenError)
 }
 
+extension MainState: Equatable {}
+func ==(lhs: MainState, rhs: MainState) -> Bool {
+    switch (lhs, rhs) {
+    case (.error(let error1), .error(let error2)): return error1 == error2
+    case (.loading, .loading), (.complete,.complete): return true
+    default:return false
+    }
+}
+
 class MainViewModel {
     var state:MainState = .loading
     
-    
+    func changeState() {
+        state = .complete
+    }
 }
+
